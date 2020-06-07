4789d0d10240f18ad884400afed68e4adacb6080
hejionglin <hejionglin@126.com>
2020-06-07 13:11:06 +0800

    添加main.c文件，用于测试GitHub存储

---
+++ test_he/main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+int main(int argc,char *argv[])
+{
+	
+	printf("argc == %d\r\n",argc);
+	printf("argv[0] == %s\r\n",argv[0]);
+	printf("argv[1] == %s\r\n",argv[1]);
+	printf("argv[2] == %s\r\n",argv[2]);
+	
+	return 0;
+}
